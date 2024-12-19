7e0652802714e5c412e1f11633c173ce84ace142
Rafael David Caraballo Vizcaino <rafaeldavid0107@gmail.com>
2024-12-18 22:58:45 -0500

    primer commit

---
+++ install-docker/install.sh
@@ -0,0 +1,8 @@
+apt-get update
+apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get install docker-ce
+usermod -a -G docker jenkins
+service jenkins restart
